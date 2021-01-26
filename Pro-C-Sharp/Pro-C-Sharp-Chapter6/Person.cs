a23c160ebae227ab1a7ef37709f0826cc0336222
Balazs Rigo <rigo.balazs86@gmail.com>
2021-01-26 20:20:04 +0100

    Finish Chapter 6!

--- Pro-C-Sharp/Pro-C-Sharp-Chapter6/Person.cs
+++ Pro-C-Sharp/Pro-C-Sharp-Chapter6/Person.cs
@@ -9,6 +9,8 @@ namespace Pro_C_Sharp_Chapter6
         public string FirstName { get; set; } = "";
         public string LastName { get; set; } = "";
         public int Age { get; set; }
+
+        public string SSN { get; set; } = "";
         public Person(string fName, string lName, int personAge)
         {
             FirstName = fName;
@@ -17,5 +19,29 @@ namespace Pro_C_Sharp_Chapter6
         }
         public Person() { }
 
+        public override string ToString() => 
+            $"[First Name: {FirstName}; Last Name: {LastName};Age: {Age}]";
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Person && obj != null)
+            {
+                Person temp;
+                temp = (Person)obj;
+                if (temp.FirstName == this.FirstName
+                && temp.LastName == this.LastName
+                && temp.Age == this.Age)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public override int GetHashCode() => ToString().GetHashCode();
     }
 }
