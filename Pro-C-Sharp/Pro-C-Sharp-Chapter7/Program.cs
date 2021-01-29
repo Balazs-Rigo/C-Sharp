c0adf09129164f529515bf8d53004dd7e075fa46
Balazs Rigo <rigo.balazs86@gmail.com>
2021-01-29 20:58:16 +0100

    Finish Chapter 7

---
+++ Pro-C-Sharp/Pro-C-Sharp-Chapter7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace Pro_C_Sharp_Chapter7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Custom Exceptions *****\n");
+            Car myCar = new Car("Rusty", 90);
+
+            try
+            {
+                // Trip Arg out of range exception.
+                myCar.Accelerate(400);
+            }
+            catch (CarIsDeadException e) when (e.ErrorTimeStamp.DayOfWeek != DayOfWeek.Monday)
+            {
+                try
+                {
+                    FileStream fs = File.Open(@"C:\carErrors.txt", FileMode.Open);
+                    
+                }
+                catch (Exception e2)
+                {
+                    // Throw an exception that records the new exception,
+                    // as well as the message of the first exception.
+                    throw new CarIsDeadException(e.Message, e2);
+                }
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pro-C-Sharp/Pro-C-Sharp-Chapter7/Radio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pro_C_Sharp_Chapter7
+{
+    public class Radio
+    {
+        public void TurnOn(bool on)
+        {
+            Console.WriteLine(on ? "Jamming..." : "Quiet time...");
+        }
+    }
+}
