c0bcf64f8e1c30f605fdc7e1d3c44612b2745691
Balazs Rigo <rigo.balazs86@gmail.com>
2021-02-19 20:44:45 +0100

    Finish Chapter 11

---
+++ Pro-C-Sharp/Pro-C-Sharp-Chapter11/AnnoyingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pro_C_Sharp_Chapter11
+{
+    public static class AnnoyingExtensions
+    {
+        public static void PrintDataAndBeep(this System.Collections.IEnumerable iterator)
+        {
+            foreach (var item in iterator)
+            {
+                Console.WriteLine(item);
+                Console.Beep();
+            }
+        }
+    }
+}
---
+++ Pro-C-Sharp/Pro-C-Sharp-Chapter11/PersonCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pro_C_Sharp_Chapter11
+{
+    public class PersonCollection
+    {
+        private Dictionary<string, Person> listPeople = new Dictionary<string, Person>();
+        // This indexer returns a person based on a string index.
+        public Person this[string name]
+        {
+            get => (Person)listPeople[name];
+            set => listPeople[name] = value;
+        }
+        public void ClearPeople()
+        { listPeople.Clear(); }
+        public int Count => listPeople.Count;
+       // IEnumerator IEnumerable.GetEnumerator() => listPeople.GetEnumerator();
+    }
+}
