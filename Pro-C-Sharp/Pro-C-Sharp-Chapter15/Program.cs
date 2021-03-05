6c94c8fd96a76b021c521b7c83814bf7d5264f7c
Balazs Rigo <rigo.balazs86@gmail.com>
2021-03-05 22:04:59 +0100

    tartok: 580 -  Understanding Late Binding

--- Pro-C-Sharp/Pro-C-Sharp-Chapter15/Program.cs
+++ Pro-C-Sharp/Pro-C-Sharp-Chapter15/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Pro_C_Sharp_Chapter15
 {
@@ -6,7 +8,105 @@ namespace Pro_C_Sharp_Chapter15
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** External Assembly Viewer *****");
+            string asmName = "";
+            Assembly asm = null;
+            do
+            {
+                Console.WriteLine("\nEnter an assembly to evaluate");
+                Console.Write("or enter Q to quit: ");
+                // Get name of assembly.
+                asmName = Console.ReadLine();
+                // Does user want to quit?
+                if (asmName.Equals("Q", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+                // Try to load assembly.
+                try
+                {
+                    asm = Assembly.Load(asmName);
+                    DisplayTypesInAsm(asm);
+                }
+                catch
+                {
+                    Console.WriteLine("Sorry, can't find assembly.");
+                }
+            } while (true);
+
+        Console.ReadLine();
+            } 
+       
+
+        static void DisplayTypesInAsm(Assembly asm)
+        {
+            Console.WriteLine("\n***** Types in Assembly *****");
+            Console.WriteLine("->{0}", asm.FullName);
+            Type[] types = asm.GetTypes();
+            foreach (Type t in types)
+                Console.WriteLine("Type: {0}", t);
+            Console.WriteLine("");
+        }
+
+        static void ListMethods(Type t)
+        {
+            Console.WriteLine("***** Methods *****");
+            MethodInfo[] mi = t.GetMethods();
+            foreach (MethodInfo m in mi)
+            {
+                // Get return type.
+                string retVal = m.ReturnType.FullName;
+                string paramInfo = "( ";
+                // Get params.
+                foreach (ParameterInfo pi in m.GetParameters())
+                {
+                    paramInfo += string.Format("{0} {1} ", pi.ParameterType, pi.Name);
+                }
+                paramInfo += " )";
+                // Now display the basic method sig.
+                Console.WriteLine("->{0} {1} {2}", retVal, m.Name, paramInfo);
+            }
+            Console.WriteLine();
+        }
+
+        static void ListFields(Type t)
+        {
+            Console.WriteLine("***** Fields *****");
+            var fieldNames = from f in t.GetFields() select f.Name;
+            foreach (var name in fieldNames)
+                Console.WriteLine("->{0}", name);
+            Console.WriteLine();
+        }
+
+        // Display property names of type.
+        static void ListProps(Type t)
+        {
+            Console.WriteLine("***** Properties *****");
+            var propNames = from p in t.GetProperties() select p.Name;
+            foreach (var name in propNames)
+                Console.WriteLine("->{0}", name);
+            Console.WriteLine();
+        }
+
+        // Display implemented interfaces.
+        static void ListInterfaces(Type t)
+        {
+            Console.WriteLine("***** Interfaces *****");
+            var ifaces = from i in t.GetInterfaces() select i;
+            foreach (Type i in ifaces)
+                Console.WriteLine("->{0}", i.Name);
+        }
+
+        // Just for good measure.
+        static void ListVariousStats(Type t)
+        {
+            Console.WriteLine("***** Various Statistics *****");
+            Console.WriteLine("Base class is: {0}", t.BaseType);
+            Console.WriteLine("Is type abstract? {0}", t.IsAbstract);
+            Console.WriteLine("Is type sealed? {0}", t.IsSealed);
+            Console.WriteLine("Is type generic? {0}", t.IsGenericTypeDefinition);
+            Console.WriteLine("Is type a class type? {0}", t.IsClass);
+            Console.WriteLine();
         }
     }
 }
