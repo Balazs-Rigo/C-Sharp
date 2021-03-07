55083f2bea19fa18823ce1b1310db70f290a03fa
Balazs Rigo <rigo.balazs86@gmail.com>
2021-03-07 13:48:59 +0100

    tartok: 623 -  Embedding Interop Metadata

---
+++ Pro-C-Sharp/Pro-C-Sharp-Chapter16/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace Pro_C_Sharp_Chapter16
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Pro-C-Sharp/Pro-C-Sharp-Chapter16/VeryDynamicClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pro_C_Sharp_Chapter16
+{
+    public class VeryDynamicClass
+    {
+        // A dynamic field.
+        private static dynamic myDynamicField;
+        // A dynamic property.
+        public dynamic DynamicProperty { get; set; }
+        // A dynamic return type and a dynamic parameter type.
+        public dynamic DynamicMethod(dynamic dynamicParam)
+        {
+            // A dynamic local variable.
+            dynamic dynamicLocalVar = "Local variable";
+            int myInt = 10;
+            if (dynamicParam is int)
+            {
+                return dynamicLocalVar;
+            }
+            else
+            {
+                return myInt;
+            }
+        }
+    }
+}
