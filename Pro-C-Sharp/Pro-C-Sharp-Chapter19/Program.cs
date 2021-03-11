991745bfc0f84f1d99d63db2afe894a9ad474afd
Balazs Rigo <rigo.balazs86@gmail.com>
2021-03-11 21:18:59 +0100

    Begin Chapter 19

---
+++ Pro-C-Sharp/Pro-C-Sharp-Chapter19/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pro_C_Sharp_Chapter19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
